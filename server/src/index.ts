501db8a9b6914559bacb3e649a2d611f1f90de64
Andres D <84483578+fmcubium@users.noreply.github.com>
2023-10-21 15:56:23 -0400

    Implemented broadCoordsCalculation and testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import { getMessages, getMessagesbyID, getMessagesbyCoordinates } from './actions/getMessages'
 import { createMessage } from './actions/createMessage'
+import { calculateBroadCoordinates } from './utilities/broadCoordsCalculation'
 
 const app = express()
 const port = 3000
@@ -47,6 +48,11 @@ app.get('/messages/:lon/:lat', async (req, res) => {
     const response = await getMessagesbyCoordinates(coordinates)
     res.json(response)
 
+})
+
+// Testing broadCoordsCalculation
+app.get("/messages/calc/:lon/:lat", async (req, res) => {
+    res.send(calculateBroadCoordinates(Number(req.params.lat), Number(req.params.lon)))
 })
 
 // ######
--- server/src/utilities/broadCoordsCalculation.ts
+++ server/src/utilities/broadCoordsCalculation.ts
@@ -1,3 +1,17 @@
+
+//Calculates latitude/longitude for who will appear in your messages
 export const calculateBroadCoordinates = (latitude: number, longitude: number) => {
-    return "FILLER TEXT";
+    let radiusEarth = 6371000
+    let metersPerDegree = 180/(1000 * Math.PI * radiusEarth)
+    let radius = 200
+    let coords = [[],[]]
+
+    //coords[0] is south/west bound, coords[1] is north/east bound
+    coords[0][0] = latitude - (radius * metersPerDegree)
+    coords[0][1] = longitude - (radius * metersPerDegree)/Math.cos(latitude * Math.PI/180)
+    
+    coords[1][0] = latitude + (radius * metersPerDegree)
+    coords[1][1] = longitude + (radius * metersPerDegree)/Math.cos(latitude * Math.PI/180)
+
+    return coords
 }
