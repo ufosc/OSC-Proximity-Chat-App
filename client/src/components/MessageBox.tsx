788c481fd1a0bd2274041dafbb5ec42d4f675a84
hinotori <71522316+h1divp@users.noreply.github.com>
2023-11-13 17:02:14 -0500

    Update MessageBox.tsx

--- client/src/components/MessageBox.tsx
+++ client/src/components/MessageBox.tsx
@@ -20,6 +20,7 @@ interface MessageBoxProps {
 }
 
 const postMessage = async (messageData: MessageDataType) => {
+  // TODO: Make the port used below (3000) part of a PORT environment variable in the .env file.
   const URL = `http://${process.env.EXPO_PUBLIC_LOCALHOST_ADDRESS}:3000/messages`;
   const response = await fetch(URL, {
     method: "POST",
