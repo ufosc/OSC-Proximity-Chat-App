bf42ecaef72ca82971993dbe97c626824f7e29ed
Alexander Wang <alexanderwang@ufl.edu>
2024-01-17 16:55:49 -0500

    adding pages

---
+++ client/src/components/Auth/LogIn.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import EmailPassword from "../Common/EmailPassword";
+import { View, Text } from "react-native";
+
+const LogIn = () => {
+  return (
+    // Logo
+    // Let's ge you started!
+
+    // Email
+    // Password
+
+    <View>
+      <Text>Log In</Text>
+    </View>
+
+    // Log In
+
+    // Make an account with Google (TEMP)
+  );
+};
+
+export default LogIn;
--- client/src/components/Common/LogInButton.tsx
+++ client/src/components/Common/LogInButton.tsx
@@ -2,30 +2,35 @@ import { router } from "expo-router";
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, Dimensions } from "react-native";
 
-const LogInButton = () => {
-    return (
-        <TouchableOpacity onPress={() => router.push("/(tests)/LogIn")} style={styles.button}>
-            <Text style={styles.button_text}>Log In</Text>
-        </TouchableOpacity>
-    );
+// Interface for props function onPress
+interface LogInButtonProps {
+  onPress?: () => void;
+}
+
+const LogInButton: React.FC<LogInButtonProps> = ({ onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.button_text}>Log In</Text>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: "#5dbea3",
-        width: Dimensions.get("window").width * 0.6,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: 10,
-    },
+  button: {
+    backgroundColor: "#5dbea3",
+    width: Dimensions.get("window").width * 0.6,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
 
-    button_text: {
-        color: "white",
-        fontWeight: "bold",
-        fontSize: Dimensions.get("window").height * 0.03,
-        padding: 18,
-    }
+  button_text: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: Dimensions.get("window").height * 0.03,
+    padding: 18,
+  },
 });
 
 export default LogInButton;
