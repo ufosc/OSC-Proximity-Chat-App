1522e78681fab58247cac761fa03d2a3cb94912b
h1divp <71522316+h1divp@users.noreply.github.com>
2024-01-18 23:57:50 -0500

    updated user type to follow new schema

--- server/src/types/User.ts
+++ server/src/types/User.ts
@@ -2,8 +2,4 @@ export interface User {
     displayName: string
     userId: string
     avatarUrl: string
-    broadLat: string
-    broadLon: string
-    specificLat: string
-    specificLon: string
 }
