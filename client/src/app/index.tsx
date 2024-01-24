7d9fd30b006f3a6af1da221d5a2415a162d3806f
Phoenix <71522316+h1divp@users.noreply.github.com>
2024-01-24 00:56:05 -0500

    Update index.tsx

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const App = () => {
   return <div>App</div>;
+  // replace this div with one of the pages inside this directory (eg. ChatroomPage in chatchannel.tsx)
 };
 
 export default App;
