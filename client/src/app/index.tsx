752568e1bc4fd2bb8a603ca20ea9ced4f6161450
Alexander Wang <alexanderwang@ufl.edu>
2024-01-25 17:50:25 -0500

    Added firebaseConfig and .env files

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { View, Text } from "react-native";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <View>
+      <Text>Loading screen</Text>
+    </View>
+  );
   // replace this div with one of the pages inside this directory (eg. ChatroomPage in chatchannel.tsx)
 };
 
--- client/src/configs/firebaseConfig.ts
+++ client/src/configs/firebaseConfig.ts
@@ -1,1 +1,17 @@
-// Replace with config from Firebase console
+import { initializeApp } from "firebase/app";
+import 'dotenv/config';
+
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
