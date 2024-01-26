14d4bb64980fb13a0bd132c0bd4b7e1a30bbfc55
Alonso <alonso.hl25@gmail.com>
2024-01-26 10:18:36 -0800

    Adding new type to docData Object

--- server/src/actions/createMessage.ts
+++ server/src/actions/createMessage.ts
@@ -4,7 +4,7 @@ import { Message } from '../types/Message'
 
 export const createMessage = async (msg:Message) => {
    const newMsgRef = doc(messages, msg.msgId)
-   const docData = {
+   const docData : Message= {
        userId: msg.userId,
        msgId: msg.msgId,
        msgContent: msg.msgContent,
