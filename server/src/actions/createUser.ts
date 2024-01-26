afbe09655bc56a9e087e46c74a4de7047dd6e86c
h1divp <71522316+h1divp@users.noreply.github.com>
2024-01-26 13:33:57 -0500

    Added type checking for createUser()

--- server/src/actions/createUser.ts
+++ server/src/actions/createUser.ts
@@ -1,10 +1,11 @@
 // Uploads a new document in the Users collection.
 import { doc, setDoc } from '@firebase/firestore'
 import { users } from '../utilities/firebaseInit'
+import { User } from '../types/User'
 
 export const createUser = async (displayName: string, userId: string, avatarUrl: string, lat: number, lon: number, geohash: string) => {
    const newMsgRef = doc(users, userId) 
-   const docData = {
+   const docData: User = {
        displayName: displayName,
        userId: userId,
        avatarUrl: avatarUrl,
