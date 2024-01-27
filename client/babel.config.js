740670f575150021cd57a95ebac9396cf7f429d9
Alexander Wang <alexanderwang@ufl.edu>
2024-01-27 15:40:12 -0500

    Fixed and added Firebase Auth

--- client/babel.config.js
+++ client/babel.config.js
@@ -2,6 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["expo-router/babel"],
+    plugins: [
+      "expo-router/babel",
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moudleName: "@env",
+          path: ".env",
+        },
+      ],
+    ],
   };
 };
