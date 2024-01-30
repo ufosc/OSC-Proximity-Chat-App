bca1d1b96bf1f5791d5bfc50d32969f6678ba8d6
h1divp <71522316+h1divp@users.noreply.github.com>
2024-01-30 17:15:01 -0500

    Fixed getting userIds (should be socket.ids) from User documents. Implemented endpoint to push messages to nearby users.
    This Users collection will be renamed to ConnectedUsers, as user information
    will be stored using a collection created by Firebase Authentication.
    
    Additionally, the method that pushes messages to users has not been rigorously tested. I'm having problems emitting to sockets
    in sequential order within the socket test files.

--- server/src/tests/socketio.test.ts
+++ server/src/tests/socketio.test.ts
@@ -2,11 +2,12 @@
 
 import { io as io } from 'socket.io-client'
 const socket_test_client_port = process.env.socket_test_client_port;
-console.log(socket_test_client_port)
+console.log("Socket clients are listening on port", socket_test_client_port)
 
 describe("socket-tests", () => {
     let socket
     let user1, user2
+    let user2Id
 
     beforeAll((done) => {
         socket = io(`http://localhost:${socket_test_client_port}`)
@@ -39,38 +40,41 @@ describe("socket-tests", () => {
             timeSent: 99999999
         }
         socket.emit('message', msgObject, (response) => {
-            console.log(response)
             expect(response).toBe('message recieved')
             done()
         })
     })
-    test('Update location', (done) => {
-        socket.emit('updateLocation', { lat: 77, lon: 77 }, (response) => {
-            expect(response).toBe('location updated')
-            done()
+    test('Update locations', (done) => {
+        const user1Coords = { lat: 29.64888, lon: -82.34420 } // Turlington Hall pin on Google Maps
+        const user2Coords = { lat: 29.64881, lon: -82.34429 } // 8.65 meters SW of user 1
+        user1.emit('updateLocation', user1Coords, (response) => {
+            expect(response).toBe("location updated")
+        }) 
+        user2.emit('updateLocation', user2Coords, (response) => {
+            expect(response).toBe("location updated")
         })
+        done()
     })
-    test('Send message to user', (done) => {
-        const user2Lat = 29.6489354
-        const user2Lon = -82.3447749 // Note slightly different lon from user1
+    test('Send message to user', async (done) => {
+        const user2Coords = { lat: 29.64881, lon: -82.34429 } // 8.65 meters SW of user 1
         const user2Message = {
             userId: user2.id,
-            messageId: "MESSAGEID",
+            messageId: "testid",
             msgContent: "omggg hi!!!! :3",
-            lat: user2Lat,
-            lon: user2Lon,
-            timeSent: 999999
+            timeSent: 999999,
+            lat: user2Coords.lat,
+            lon: user2Coords.lon
         }
-
-        user1.emit('updateLocation', {lat: 29.6489354, lon: -82.3441252}) // Turlington Plaza
-        user2.emit('updateLocation', {lat: user2Lat, lon: user2Lon })
-        user1.on('message', (message, ack) => {
+        let res = ""
+        await user1.on('message', (message, response) => {
             console.log(`User 2 recieved message ${message}`)
-            ack("hii")
+            console.log(response)
+            expect(message).toBe("omggg hi!!!! :3")
         })
-        user2.emit('message', user2Message, (response) => {
-            expect(response).toBe("hii")
-            done()
-        })
+        if (res == "location updated") {
+            user2.emit('message', user2Message, (response) => {
+                console.log(response)
+            })
+        }
     })
 })
