227f2e4dd9068fa4161f94d68ffa980b8f39a217
Aadit Kamat <30969577+aaditkamat@users.noreply.github.com>
2024-02-03 17:42:22 -0500

    Modularize NearbyHeader component

--- client/src/components/Common/NearbyHeader.tsx
+++ client/src/components/Common/NearbyHeader.tsx
@@ -1,35 +1,25 @@
 
 import { View, Text, StyleSheet, Dimensions, Image } from 'react-native'
 import React from 'react'
+import NearbyCount from './NearbyCount';
 
 interface CounterProps {
     count: number;
-  }
+}
 
 export const NearbyHeader: React.FC<CounterProps> = ({ count }) => {
     return (
         <View style={styles.nearbyContainer}>
-        <View style={{flexDirection: "row"}}>
-        <Text style={{
-                fontSize: 30,
-                fontWeight: 'bold',
-                color: 'black',
-                textAlign:"left",
-                width: "35%",
-                marginLeft: "5%",
-            }}>Nearby</Text>
-            <View style={styles.iconContainer}>
-                <View style={styles.iconAndCount}>
-            <Image style={styles.iconImage} source={require("../../../assets/nearby_icon.png")}/>
-            <Text style={{
-                fontSize: 20,
-                fontWeight: 'bold',
-                color: 'black',
-                textAlign:"center",
-                width: "50%",
-            }}>{count}</Text>
-            </View>
-            </View>
+            <View style={{flexDirection: "row"}}>
+                <Text style={{
+                    fontSize: 30,
+                    fontWeight: 'bold',
+                    color: 'black',
+                    textAlign:"left",
+                    width: "35%",
+                    marginLeft: "5%",
+                }}>Nearby</Text>
+                <NearbyCount count={count} />
             </View>
         </View>
     )
@@ -44,19 +34,7 @@ export const NearbyHeader: React.FC<CounterProps> = ({ count }) => {
     flexDirection:'row',
     alignItems: "center",
     
-  },
-  iconImage:{
-    height: Dimensions.get('window').height * 0.025,
-    width: Dimensions.get('window').height * 0.03,
-    
-    },
-    iconContainer:{
-        width: "50%"
-    },
-    iconAndCount:{
-        alignSelf: "flex-end",
-        alignItems: "center"
-    }
+  }
 });
 
 export default NearbyHeader;
