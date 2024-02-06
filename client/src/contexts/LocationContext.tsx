cd401d5c4129095cf5b26b459669894abda8a9e3
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-02-06 16:56:32 -0500

    moved ip and interval to env

--- client/src/contexts/LocationContext.tsx
+++ client/src/contexts/LocationContext.tsx
@@ -1,65 +1,81 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import * as Location from "expo-location";
+import { LOCATION_REFRESH_RATE } from "@env";
 
 interface LocationContextProps {
-    longitude: number;
-    latitude: number;
-    isLocationEnabled: boolean;
+  longitude: number;
+  latitude: number;
+  isLocationEnabled: boolean;
 }
 
 interface LocationType {
-    longitude: number;
-    latitude: number;
+  longitude: number;
+  latitude: number;
 }
 
 const LocationContext = createContext<LocationContextProps | null>(null);
 
 export const useLocation = () => {
-    return useContext(LocationContext);
+  return useContext(LocationContext);
 };
 
-export const LocationProvider = ({ children }: { children: React.ReactNode }) => {
-    const [location, setLocation] = useState<LocationType>({
-        latitude: 99999, // Impossible starting value
-        longitude: 99999,
-    });
-    const [isLocationEnabled, setIsLocationEnabled] = useState(false);
+export const LocationProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [location, setLocation] = useState<LocationType>({
+    latitude: 99999, // Impossible starting value
+    longitude: 99999,
+  });
+  const [isLocationEnabled, setIsLocationEnabled] = useState(false);
 
-    useEffect(() => {
-        (async () => {
-            // Request location permissions, if not granted, return
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== "granted") {
-                console.log("Permission to access location was denied");
-                return;
-            }
+  useEffect(() => {
+    (async () => {
+      // Request location permissions, if not granted, return
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission to access location was denied");
+        return;
+      }
 
-            setIsLocationEnabled(true);
+      setIsLocationEnabled(true);
 
-            const interval = setInterval(async () => {
-                try {
-                    let locationData = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Highest }); // High accuracy for now for testing!
-                    if (locationData.coords.latitude !== location.latitude || locationData.coords.longitude !== location.longitude) {
-                        setLocation({
-                            latitude: locationData.coords.latitude,
-                            longitude: locationData.coords.longitude,
-                        });
-                    } else {
-                        console.log("Location has not changed");
-                    }
-                } catch (error) {
-                    console.error("Error fetching location:", error);
-                }
-            }, 3000); // Fetch location every 3 seconds
+      const interval = setInterval(async () => {
+        try {
+          let locationData = await Location.getCurrentPositionAsync({
+            accuracy: Location.Accuracy.Highest,
+          }); // High accuracy for now for testing!
+          if (
+            locationData.coords.latitude !== location.latitude ||
+            locationData.coords.longitude !== location.longitude
+          ) {
+            setLocation({
+              latitude: locationData.coords.latitude,
+              longitude: locationData.coords.longitude,
+            });
+          } else {
+            console.log("Location has not changed");
+          }
+        } catch (error) {
+          console.error("Error fetching location:", error);
+        }
+      }, LOCATION_REFRESH_RATE); // Fetch location every 3 seconds
 
-            // Cleanup function to clear interval when component unmounts
-            return () => clearInterval(interval);
-        })();
-    }, []); 
+      // Cleanup function to clear interval when component unmounts
+      return () => clearInterval(interval);
+    })();
+  }, []);
 
-    return (
-        <LocationContext.Provider value={{ longitude: location.longitude, latitude: location.latitude, isLocationEnabled: isLocationEnabled }}>
-            {children}
-        </LocationContext.Provider>
-    );
+  return (
+    <LocationContext.Provider
+      value={{
+        longitude: location.longitude,
+        latitude: location.latitude,
+        isLocationEnabled: isLocationEnabled,
+      }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
 };
