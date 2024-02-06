a0fb51299c08d7ee0c94899947b77616cbddc65f
Andres D <84483578+fmcubium@users.noreply.github.com>
2024-02-06 16:55:10 -0500

    Updated client/config_example.md to be consistent with backend (#120)
    * Updated config_example.md to be consistent w backe
    
    * Updated backend config to match frontend for consistency
    
    ---------
    
    Co-authored-by: h1divp <71522316+h1divp@users.noreply.github.com>

--- server/src/utilities/firebaseInit.ts
+++ server/src/utilities/firebaseInit.ts
@@ -3,12 +3,12 @@ import { getFirestore, CollectionReference, collection, DocumentData } from 'fir
 import 'dotenv/config';
 
 export const firebaseApp = initializeApp({
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 })
 
 export const firestore = getFirestore()
