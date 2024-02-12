de2b020c7bdbf713aff20c9267e6bf22b0326d5b
Phantom0110 <mohamali9173@gmail.com>
2024-02-11 20:54:01 -0500

    Added test for creating user for issue #92

--- server/src/tests/express.test.ts
+++ server/src/tests/express.test.ts
@@ -1,27 +1,110 @@
 import {describe, expect, test} from '@jest/globals';
+import axios from 'axios';
 
-describe("Rest Tests", () => {
-    test("Create user", async () => {
-        const messageData = {
-            displayName: "USERNAME",
-            userId: "USERID",
-            avatarUrl: "AVATARURL",
-            lat: 100,
-            lon: 100,
-            geohash: "GEOHASH"
-        }
-        const response = await fetch('localhost:3001/users', {
-            method: "POST",
-            mode: "no-cors",
-            headers: {
-            "Content-Type": "application/json",
-            },
-            referrerPolicy: "same-origin",
-            body: JSON.stringify(messageData),
-        });
-        console.log(response.json())
-        // expect(response).toBe("Operation <POST /user> was handled successfully.")
-    })
+async function RestPost(messageData: any) {
+    try {
+      const response = await axios.post('http://localhost:3001/users', messageData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+
+jest.mock('axios')
+
+describe('Rest tests', () => {
+    test('Create user successfully', async () => {
+      const messageData: any = { displayName: "USERNAME",
+        userId: "USERID",
+        avatarUrl: "AVATARURL",
+        lat: 100,
+        lon: 100,
+        geohash: "GEOHASH" 
+    };
+      const expectedResponse = 'Operation <POST /users> was handled successfully';
+  
+      (axios.post as jest.MockedFunction<typeof axios.post>).mockResolvedValue({
+        data: expectedResponse,
+      });
+  
+      const result = await RestPost(messageData);
+  
+      expect(result).toEqual(expectedResponse);
+      
+    });
+  
+    test('Create user failed', async () => {
+      const messageData: any = { displayName: "USERNAME",
+        userId: "USERID",
+        avatarUrl: "AVATARURL",
+        lat: 100,
+        lon: 100,
+        geohash: "GEOHASH" 
+    };
+  
+      (axios.post as jest.MockedFunction<typeof axios.post>).mockRejectedValue(
+        new Error('Simulated API error')
+      );
+      
+      await expect(RestPost(messageData)).rejects.toThrow('Simulated API error');
+    });
+  });
+
+// test("Create user", async () => {
+//     const messageData = {
+//         displayName: "USERNAME",
+//         userId: "USERID",
+//         avatarUrl: "AVATARURL",
+//         lat: 100,
+//         lon: 100,
+//         geohash: "GEOHASH"
+//     }
+
+    
+
+    
+
+    // const response = await fetch('localhost:3001/users', {
+    //     method: "POST",
+    //     headers: {
+    //     "Content-Type": "application/json",
+    //     },
+    //     referrerPolicy: "same-origin",
+    //     body: JSON.stringify(messageData),
+    // });
+    
+    
+
+
+// describe("Rest Tests", () => {
+//     test("Create user", async () => {
+//         const messageData = {
+//             displayName: "USERNAME",
+//             userId: "USERID",
+//             avatarUrl: "AVATARURL",
+//             lat: 100,
+//             lon: 100,
+//             geohash: "GEOHASH"
+//         }
+//         const response = await fetch('localhost:3001/users', {
+//             method: "POST",
+//             headers: {
+//             "Content-Type": "application/json",
+//             },
+//             referrerPolicy: "same-origin",
+//             body: JSON.stringify(messageData),
+//         });
+//         const responseJson = await response.json()
+//         console.log(responseJson)
+
+        // expect(response.json()).toBe("Operation <POST /user> was handled successfully.")
+    // })
 
     // test("toggleConnection", async () => {
     //     const URL = "localhost:3001/users?userId=USERID\&toggleConnection=true"
@@ -62,4 +145,3 @@ describe("Rest Tests", () => {
     //         })
     //     expect(response).toBe("Operation <DELETE /users?userId> was handled successfully.")
     // })
-})
