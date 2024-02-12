bbcf8f9c1e15f17651619623534c108ca3c5a88d
Phantom0110 <mohamali9173@gmail.com>
2024-02-11 21:51:07 -0500

    Added rest API tests. When testing failed  cases, changing the expected throw response to something it shouldn't be causes the test to still pass. Issue #92

--- server/src/tests/express.test.ts
+++ server/src/tests/express.test.ts
@@ -13,8 +13,35 @@ async function RestPost(messageData: any) {
     } catch (error) {
       throw error;
     }
-  }
+}
   
+async function RestConnection() {
+    try {
+        const response = await axios.put('localhost:3001/users?userId=USERID\&toggleConnection=true')
+        return response.data;
+    } catch (error) {
+        throw error;
+    } 
+}
+
+async function RestCoordinates() {
+    try {
+        const response = await axios.put('localhost:3001/users?userId=USERID\&lat=88\&lon=88')
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function RestDeleteUser() {
+    try {
+        const response = await axios.delete('localhost:3001/users?userId=USERID')
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
 
 jest.mock('axios')
 
@@ -54,31 +81,59 @@ describe('Rest tests', () => {
       
       await expect(RestPost(messageData)).rejects.toThrow('Simulated API error');
     });
+
+    test('toggleConnection successful', async () => {
+        const expectedResponse = 'Operation <PUT /user?userId&lat&lon> was handled successfully.';
+
+        (axios.put as jest.MockedFunction<typeof axios.put>).mockResolvedValue({
+            data: expectedResponse,
+        });
+        const result = await RestConnection();
+        expect(result).toEqual(expectedResponse)
+    })
+
+    test('toggleConnection failed', async () => {
+        (axios.put as jest.MockedFunction<typeof axios.put>).mockRejectedValue(
+            new Error('toggleConnection failed')
+        );
+        await expect(RestConnection()).rejects.toThrow('toggleConnection failed')
+    });
+
+    test('update coordinates', async () => {
+        const expectedResponse = 'Operation <PUT /user?userId&lat&lon> was handled successfully.';
+        (axios.put as jest.MockedFunction<typeof axios.put>).mockResolvedValue({
+            data: expectedResponse,
+        });
+        const result = await RestCoordinates();
+        expect(result).toEqual(expectedResponse)
+    })
+
+    test('update coordinates failed', async () => {
+        (axios.put as jest.MockedFunction<typeof axios.put>).mockRejectedValue(
+            new Error('update coordinates failed')
+        );
+        await expect(RestCoordinates()).rejects.toThrow('update coordinates failed')
+    })
+
+    test('Delete user', async () => {
+        const expectedResponse = 'Operation <DELETE /users?userId> was handled successfully.';
+        (axios.delete as jest.MockedFunction<typeof axios.delete>).mockResolvedValue({
+            data: expectedResponse,
+        })
+        const result = await RestDeleteUser();
+        expect(result).toEqual(expectedResponse)
+    })
+
+    test('Delete user failed', async () => {
+        (axios.delete as jest.MockedFunction<typeof axios.delete>).mockRejectedValue(
+            new Error('Delete user failed')
+        );
+        await expect(RestDeleteUser()).rejects.toThrow('Delete user failed')
+    })
   });
 
-// test("Create user", async () => {
-//     const messageData = {
-//         displayName: "USERNAME",
-//         userId: "USERID",
-//         avatarUrl: "AVATARURL",
-//         lat: 100,
-//         lon: 100,
-//         geohash: "GEOHASH"
-//     }
 
-    
 
-    
-
-    // const response = await fetch('localhost:3001/users', {
-    //     method: "POST",
-    //     headers: {
-    //     "Content-Type": "application/json",
-    //     },
-    //     referrerPolicy: "same-origin",
-    //     body: JSON.stringify(messageData),
-    // });
-    
     
 
 
