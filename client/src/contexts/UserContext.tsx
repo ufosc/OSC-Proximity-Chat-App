259dacb4687f15344cd5e4c07a5e05e73c82004a
AaronGibson2 <a.gibson1@ufl.edu>
2024-02-13 17:33:10 -0500

    moved user and display name generation into UserProvider

--- client/src/contexts/UserContext.tsx
+++ client/src/contexts/UserContext.tsx
@@ -6,18 +6,8 @@ import { generateName } from '../utils/scripts';
 const UserContext = createContext<UserType>({
     userID: "something",
     displayName: "default",
-    pfp: "null"
+    pfp: "picture"
 });
-
-const [user, setUser] = useState<UserType>({
-    userID: "something",
-    displayName: "default",
-    pfp: "null"
-});
-
-useEffect(() => {
-    user.displayName = generateName();
-}, []);
 
 export const useUser = () => {
     return useContext(UserContext);
@@ -28,8 +18,23 @@ export const changeUsername = (newUsername: string) => {
     setUser
 }
 */
+const user = useUser();
 
 export const UserProvider = ({ children }: {children: React.ReactNode}) => {
+    // const [userID, setUserID] = useState<string>();
+    // const [displayName, setDisplayName] = useState<string>();
+    // const [pfp, setPfp] = useState<string>();
+
+    const [user, setUser] = useState<UserType>({
+        userID: "something",
+        displayName: "default",
+        pfp: "picture"
+    })
+
+    useEffect(() => {
+        user.displayName = generateName();
+    }, []);
+
     return (
         <UserContext.Provider value={user}>
             {children}
