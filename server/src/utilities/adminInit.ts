17e6ba017e42cda12b72ef4752401b351feb4500
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-02-17 21:37:52 -0500

    initializing firestore in admin

--- server/src/utilities/adminInit.ts
+++ server/src/utilities/adminInit.ts
@@ -1,9 +1,12 @@
 const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 const admin = require('firebase-admin');
-const serviceAccount = require("../../src/private_key/<YOUR KEY>.json");
+const serviceAccount = require("../../src/private_key/private.json");
 
 export const adminApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
+
+export const db = admin.firestore();
 
 console.log("[FIREBASE-ADMIN] Firebase admin SDK synced.")
