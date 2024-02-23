202b5bdffd0c9233a126b3084c82eb5840baa7d9
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2024-02-23 16:26:39 -0500

    Refactored most of the actions (#156)
    * new branch + function fix
    
    * small bug fixes
    
    * fixes pt2
    
    * JWT WIP
    
    * new branch + function fix
    
    * small bug fixes
    
    * fixes pt2
    
    * JWT WIP
    
    * new branch + function fix
    
    * small bug fixes
    
    * fixes pt2
    
    * JWT WIP
    
    * new branch + function fix
    
    * small bug fixes
    
    * added passport
    
    * started auth + admin sdk
    
    * removed passport
    
    * added private key to gitignore
    
    * Delete server/private_keys/private.json
    
    * initializing firestore in admin
    
    * finished most of action refactoring
    
    * Made error case for email in use display correctly in front end (#158)
    
    * changed getConnectedUsers to admin
    
    * migrated to firebase-admin
    
    ---------
    
    Co-authored-by: AlexanderWangY <alexanderwang@ufl.edu>
    Co-authored-by: Mohammed Ali <146048575+Phantom0110@users.noreply.github.com>

--- server/src/actions/deleteConnectedUser.ts
+++ server/src/actions/deleteConnectedUser.ts
@@ -1,20 +1,9 @@
 // Delete a ConnectedUser document given a document's index. This should typically be a socketId, but it can also be something else.
-import { doc, getDoc, deleteDoc } from '@firebase/firestore'
-import { connectedUsers } from '../utilities/firebaseInit'
+import { connectedUsersCollection } from '../utilities/adminInit'
 
-export const deleteConnectedUserByIndex = async (index: string) => {
+export const deleteConnectedUserByUID = async (socketID: string) => {
   try {
-    const userRef = doc(connectedUsers, index)
-
-    // The promise returned by deleteDoc will be fulfilled (aka return 'true') both if the document requested for deletion exists or doesn't exist. It is rejected if the program is unable to send this request to Firestore.
-    // Therefore, we need to check to see if the document exists first, to most accurately know if it will be deleted.
-    // However, technically, there could be some kind of failure by deleteDoc after this check is performed, where the status of the deletion would then be inaccurately returned.
-    // TODO: find a way to assuredly know if a document is deleted after deleteDoc is called.
-
-    const userDoc = await getDoc(userRef)
-    if (!userDoc.exists()) throw Error("[FIREBASE] User does not exist.")
-
-    await deleteDoc(userRef)
+    await connectedUsersCollection.doc(socketID).delete()
     return true
         
   } catch (error) {
