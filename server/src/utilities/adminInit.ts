b11a1cf25f451ff26feb36875f4b94d308ae119a
h1divp <71522316+h1divp@users.noreply.github.com>
2024-02-28 17:39:32 -0500

    Remade collection name changes

--- server/src/utilities/adminInit.ts
+++ server/src/utilities/adminInit.ts
@@ -6,7 +6,10 @@ export const adminApp = admin.initializeApp({
 });
 
 export const db = admin.firestore();
-export const connectedUsersCollection = db.collection('users');
-export const messagesCollection = db.collection('messages');
+export const connectedUsersCollection = db.collection('ConnectedUsers');
+export const messagesCollection = db.collection('Messages');
 
 console.log("[FIREBASE-ADMIN] Firebase admin SDK synced.")
+
+// TODO: refactor file name to 'firebase' for accuracy.
+// TODO: move key info to .env
