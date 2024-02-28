00c0095b59472544507a23f169318b14a7a228c7
h1divp <71522316+h1divp@users.noreply.github.com>
2024-02-28 17:31:42 -0500

    Added socketIo.connect()

--- client/src/contexts/SocketContext.tsx
+++ client/src/contexts/SocketContext.tsx
@@ -31,6 +31,7 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
         }
       });
 
+      socketIo.connect()
       setSocket(socketIo);
       setMounted(true);
     }
