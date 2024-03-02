7daf4ba8fddbb991b5741c3bbfde4be4c7988f26
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-03-02 18:50:34 -0500

    replaced stack with tabs

--- client/src/app/(home)/_layout.tsx
+++ client/src/app/(home)/_layout.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 import { SettingsProvider } from "../../contexts/SettingsContext";
 import { SocketProvider } from "../../contexts/SocketContext";
 import { LocationProvider } from "../../contexts/LocationContext";
 import { UserProvider } from "../../contexts/UserContext";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 const AuthLayout = () => {
   return (
@@ -11,13 +12,22 @@ const AuthLayout = () => {
       <SocketProvider>
         <UserProvider>
           <SettingsProvider>
-            <Stack
-              screenOptions={{
-                headerShown: false,
-              }}
-            >
-              <Stack.Screen name="chatchannel" options={{}} />
-            </Stack>
+            <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+              <Tabs.Screen
+                name="chatchannel"
+                options={{
+                  title: 'Chat',
+                  tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+                }}
+              />
+              <Tabs.Screen
+                name="settings"
+                options={{
+                  title: 'Settings',
+                  tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+                }}
+              />
+            </Tabs>
           </SettingsProvider>
         </UserProvider>
       </SocketProvider>
---
+++ client/src/app/(home)/settings.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import SettingsScreen from '../../components/Settings/SettingsScreen';
+
+const SettingsPage = () => {
+  return (
+    <SettingsScreen />
+  )
+}
+
+export default SettingsPage;
