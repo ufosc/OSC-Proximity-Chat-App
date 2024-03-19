0630ec4b826c1e91a0bd46d0ed545ddc5628f7ac
h1divp <71522316+h1divp@users.noreply.github.com>
2024-03-19 18:07:09 -0400

    Re-added env import

--- client/src/configs/firebaseConfig.ts
+++ client/src/configs/firebaseConfig.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { initializeAuth, getReactNativePersistence, getAuth, Auth } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import {API_KEY, AUTH_DOMAIN} from "@env"; // Don't worry about this env error!
 
 const firebaseConfig = {
   apiKey: API_KEY || "Mock-Key",
