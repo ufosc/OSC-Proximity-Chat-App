787e006a9bd73f845626263e972e0207c2c41bb1
Dylan Coben <59846502+dyland88@users.noreply.github.com>
2024-04-01 00:22:02 -0400

    Sign Up Page (#204)
    * Fixed input prop types
    
    * Changed login text and text entry for password confirmation
    
    * Changed login text and text entry for password confirmation
    
    * Fixed fonts not working
    
    * Removed redundant signupinput methods
    
    * Updated input container to use gap instead of height
    
    * Separated email errors and password errors
    
    * Added extra error case and sign up button
    
    * Renamed externalloginbutton to externalauthbutton
    
    * Removed unused code and imports
    
    * Removed unused email import

--- client/src/components/Auth/SignUpScreen.tsx
+++ client/src/components/Auth/SignUpScreen.tsx
@@ -1,42 +1,56 @@
-import React, { useEffect } from "react";
 import { useFonts } from "expo-font";
 import {
   View,
   Text,
   StyleSheet,
   Dimensions,
-  KeyboardAvoidingView,
-  Platform,
   TouchableWithoutFeedback,
   Keyboard,
+  TouchableOpacity,
 } from "react-native";
-import { SignUpEmailInput, SignUpPasswordInput, SignUpConfirmPasswordInput } from "../Common/CustomInputs";
-import SignUpButton from "../Common/SignUpButton";
-import { AuthenticationErrorMessage, AuthenticationResponse, CustomError } from "./AuthenticationResponse";
+import {
+  EmailInput,
+  PasswordInput,
+  ConfirmPasswordInput,
+} from "../Common/CustomInputs";
+
+import {
+  AuthenticationErrorMessage,
+  AuthenticationResponse,
+  CustomError,
+} from "./AuthenticationResponse";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { appSignUp } from "../../services/store";
+import { ArrowLeftCircle } from "react-native-feather";
+import { SignUpButton, ExternalAuthButton } from "../Common/AuthButtons";
+import React from "react";
 
 const SignUpScreen = () => {
   const router = useRouter();
   const [fontsLoaded, fontError] = useFonts({
-    "Gilroy-ExtraBold": require("../../../assets/fonts/Gilroy-ExtraBold.otf"),
-    "Gilroy-Light": require("../../../assets/fonts/Gilroy-Light.otf"),
+    "Quicksand-Bold": require("../../../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-Medium": require("../../../assets/fonts/Quicksand-Medium.ttf"),
   });
 
-  const { inputEmail } = useLocalSearchParams();
   const [email, setEmail] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
   const [confirmPassword, setConfirmPassword] = React.useState<string>("");
-  const [authResponse, setAuthResponse] = React.useState<AuthenticationResponse>();
+  const [authResponse, setAuthResponse] =
+    React.useState<AuthenticationResponse>();
+  //grays out password on password error
+  const [invalidPassword, invalidatePassword] = React.useState<boolean>(false);
+  //grays out email on email error
+  const [invalidEmail, invalidateEmail] = React.useState<boolean>(false);
 
   const onHandleSubmit = async () => {
     Keyboard.dismiss();
     // Check if password and confirm password match
     if (password !== confirmPassword) {
-        const nonmatching_password_error: AuthenticationResponse = {
-        error: new CustomError("Invalid password", "Passwords do not match")
-      }
-      setAuthResponse(nonmatching_password_error)
+      const nonmatching_password_error: AuthenticationResponse = {
+        error: new CustomError("Invalid password", "Passwords do not match"),
+      };
+      invalidatePassword(true);
+      setAuthResponse(nonmatching_password_error);
 
       return;
     }
@@ -53,44 +67,128 @@ const SignUpScreen = () => {
   if (!fontsLoaded && !fontError) {
     return null;
   }
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  const handleGoogleSignUp = async () => {
+    console.log("Google Sign Up");
+  };
+
+  const handleFacebookSignIn = async () => {
+    console.log("Facebook Sign Up");
+  };
+
+  const handleAppleSignUp = async () => {
+    console.log("Apple Sign Up");
+  };
+
+  const handleGithubSignUp = async () => {
+    console.log("Github Sign Up");
+  };
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View>
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : undefined}
-        >
-          <View style={styles.main_container}>
-            <View style={styles.header_container}>
-              <Text style={styles.header_text}>Welcome!</Text>
-            </View>
-            <View style={styles.input_container}>
-              <SignUpEmailInput
-                value={email}
-                onChangeText={(text) => setEmail(text)}
-              />
-              <SignUpPasswordInput
-                value={password}
-                onChangeText={(text) => setPassword(text)}
-              />
-              <SignUpConfirmPasswordInput
-                value={confirmPassword}
-                onChangeText={(text) => setConfirmPassword(text)}
-              />
-            </View>
-            <View style={styles.button_container}>
-              <SignUpButton onPress={onHandleSubmit} />
-            </View>
+        <View style={styles.main_container}>
+          <TouchableOpacity
+            onPress={() => router.back()}
+            style={styles.back_button}
+          >
+            <ArrowLeftCircle
+              color={"black"}
+              strokeWidth={1.4}
+              width={34}
+              height={34}
+            />
+          </TouchableOpacity>
+          <View style={styles.header_container}>
+            <Text style={styles.header_text}>Join the conversation</Text>
+            <Text style={styles.subheader_text}>Talk to people near you</Text>
           </View>
-        </KeyboardAvoidingView>
+          <View style={styles.input_container}>
+            <EmailInput
+              value={email}
+              onChangeText={(text) => {
+                invalidateEmail(false);
+                setEmail(text);
+              }}
+              invalid={invalidEmail}
+            />
+            <PasswordInput
+              value={password}
+              onChangeText={(text) => {
+                invalidatePassword(false);
+                setPassword(text);
+              }}
+              invalid={invalidPassword}
+            />
+            <ConfirmPasswordInput
+              value={confirmPassword}
+              onChangeText={(text) => setConfirmPassword(text)}
+              invalid={invalidPassword}
+            />
+          </View>
+          <View style={styles.button_container}>
+            <SignUpButton onPress={onHandleSubmit} />
+          </View>
 
-        <View style={styles.error_container}>
-          <AuthenticationErrorMessage response={authResponse} onPress={() => setAuthResponse(undefined)} />
+          <View style={styles.divider_container}>
+            <View style={styles.horizontal_line} />
+            <View>
+              <Text style={[styles.regular_text, { marginHorizontal: 10 }]}>
+                Or Login With
+              </Text>
+            </View>
+            <View style={styles.horizontal_line} />
+          </View>
+
+          <View style={styles.externalLinkContainer}>
+            <ExternalAuthButton
+              onPress={handleGoogleSignUp}
+              companyName="google"
+            />
+            <ExternalAuthButton
+              onPress={handleAppleSignUp}
+              companyName="apple"
+            />
+            <ExternalAuthButton
+              onPress={handleFacebookSignIn}
+              companyName="facebook"
+            />
+            <ExternalAuthButton
+              onPress={handleGithubSignUp}
+              companyName="github"
+            />
+          </View>
+          <View style={styles.footer_text_container}>
+            <Text style={styles.footer_text}>Already have an account?</Text>
+            <TouchableOpacity
+              onPress={() => router.replace({ pathname: "/login" })}
+            >
+              <Text
+                style={[
+                  styles.footer_text,
+                  { color: "#5dbea3", textDecorationLine: "underline" },
+                ]}
+              >
+                Log in
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
 
+        <View style={styles.error_container}>
+          <AuthenticationErrorMessage
+            response={authResponse}
+            onPress={() => setAuthResponse(undefined)}
+          />
+        </View>
       </View>
     </TouchableWithoutFeedback>
   );
+
+
 };
 
 const styles = StyleSheet.create({
@@ -98,8 +196,21 @@ const styles = StyleSheet.create({
     display: "flex",
     height: "100%",
     width: "100%",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
+    paddingHorizontal: Dimensions.get("window").width * 0.11,
+    paddingVertical: Dimensions.get("window").height * 0.01,
+    backgroundColor: "white",
+    gap: Dimensions.get("window").height * 0.029,
+  },
+
+  error_container: {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "100%",
+    top: Dimensions.get("window").height * 0.1,
+    position: "absolute",
   },
 
   input_container: {
@@ -107,8 +218,7 @@ const styles = StyleSheet.create({
     width: "100%",
     justifyContent: "space-around",
     alignItems: "center",
-    height: Dimensions.get("window").height * 0.2,
-    marginBottom: 20,
+    gap: Dimensions.get("window").height * 0.02,
   },
 
   button_container: {
@@ -118,25 +228,63 @@ const styles = StyleSheet.create({
     width: "100%",
   },
 
-  error_container: {
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-    width: "100%",
-    bottom: Dimensions.get("window").height * 0.10,
-    position: "absolute",
-  },
-
   header_container: {
     display: "flex",
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "flex-start",
     width: "100%",
+    marginBottom: Dimensions.get("window").height * 0.019,
+    marginTop: Dimensions.get("window").height * 0.17,
   },
 
   header_text: {
-    fontFamily: "Gilroy-ExtraBold",
-    fontSize: 30,
+    fontFamily: "Quicksand-Bold",
+    fontSize: 37,
+    marginBottom: Dimensions.get("window").height * 0.01,
+  },
+  subheader_text: {
+    fontFamily: "Quicksand-Medium",
+    fontSize: 20,
+  },
+  regular_text: {
+    fontFamily: "Quicksand-Medium",
+    color: "#8E8E8E",
+  },
+  horizontal_line: {
+    flex: 1,
+    height: 1,
+    backgroundColor: "#8E8E8E",
+  },
+  externalLinkContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  },
+  footer_text: {
+    fontFamily: "Quicksand-Bold",
+    color: "black",
+    fontSize: 15,
+  },
+  footer_text_container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    gap: 7,
+    flex: 1,
+    paddingBottom: Dimensions.get("window").height * 0.01,
+  },
+  divider_container: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: Dimensions.get("window").height * 0.011,
+  },
+  back_button: {
+    position: "absolute",
+    top: Dimensions.get("window").height * 0.075,
+    left: Dimensions.get("window").width * 0.075,
   },
 });
 
