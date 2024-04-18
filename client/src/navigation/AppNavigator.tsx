1c256812a5311f6060d858bafeede4243880a2fb
Mohammed Ali <mohamali9173@gmail.com>
2024-04-18 17:56:20 -0400

    recovered

--- client/src/navigation/AppNavigator.tsx
+++ client/src/navigation/AppNavigator.tsx
@@ -1,10 +1,21 @@
 import * as React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import  {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  Image,
+  View,
+} from 'react-native';
 import ChatScreen from "../screens/chat/ChatScreen";
 import SettingsScreen from "../screens/settings/SettingsScreen";
 import { SocketProvider } from "../contexts/SocketContext";
 import { LocationProvider } from "../contexts/LocationContext";
 import { UserProvider } from "../contexts/UserContext";
+import { Map, Settings, MessageSquare } from "react-native-feather";
 
 const Tab = createBottomTabNavigator();
 
@@ -15,10 +26,83 @@ const AppNavigator = () => {
         <UserProvider>
           <Tab.Navigator screenOptions={{
             headerShown: false,
-          
+            tabBarStyle: {
+              backgroundColor: '#EFEFEF',
+              borderTopLeftRadius: 30,
+              borderTopRightRadius: 30,
+            },
+            
           }}>
-            <Tab.Screen name="Home" component={ChatScreen} />
-            <Tab.Screen name="Profile" component={SettingsScreen} />
+            <Tab.Screen 
+            name="Map"
+            component={ChatScreen}
+            options={{
+              tabBarShowLabel: false,
+              tabBarActiveTintColor: '#34D1BF',
+              tabBarIcon: ({focused}) => (
+                <View style={{alignItems: 'center', justifyContent: 'center', top: 3, left: 5}}>
+                  <Map
+                    color={'black'}
+                    style={{
+                      width: 22,
+                      height: 22,
+                    }}
+                  />
+                  <Text
+                  style={{color: focused ? '#34D1BF' : 'black', fontSize: 10, top: 2,}}>
+                    Map
+                  </Text>
+                
+                </View>
+              )
+            }}
+            />
+            <Tab.Screen 
+            name="Chat" 
+            component={ChatScreen}
+            options={{
+              tabBarShowLabel: false,
+              tabBarActiveTintColor: '#34D1BF',
+              tabBarIcon: ({focused}) => (
+                <View style={{alignItems: 'center', justifyContent: 'center', top: 3}}>
+                  <MessageSquare
+                    color={'black'}
+                    style={{
+                      width: 22,
+                      height: 22,
+                    }}
+                  />
+                  <Text
+                  style={{color: focused ? '#34D1BF' : 'black', fontSize: 10, top: 2}}>
+                    Chat
+                  </Text>
+                
+                </View>
+              )
+            }} />
+            <Tab.Screen 
+            name="Settings" 
+            component={SettingsScreen}
+            options={{
+              tabBarShowLabel: false,
+              tabBarActiveTintColor: '#34D1BF',
+              tabBarIcon: ({focused}) => (
+                <View style={{alignItems: 'center', justifyContent: 'center', top: 3, right: 5}}>
+                  <Settings
+                    color={'black'}
+                    style={{
+                      width: 22,
+                      height: 22,
+                    }}
+                  />
+                <Text
+                  style = {{color: focused ? '#34D1BF' : 'black', fontSize: 10, top: 2}}>
+                  Settings
+                  </Text>
+                </View>
+              )
+            }}
+            />
           </Tab.Navigator>
         </UserProvider>
       </SocketProvider>
