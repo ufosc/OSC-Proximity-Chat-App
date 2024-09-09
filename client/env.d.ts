735e163a7f49c9f99cb25f0955d7940692468955
RupertJonesSA <aljamal.sami05@gmail.com>
2024-09-09 19:53:30 -0400

    fixed env linting errors

---
+++ client/env.d.ts
@@ -0,0 +1,10 @@
+declare module "@env" {
+  export const EXPO_IP: string;
+  export const API_KEY: string;
+  export const AUTH_DOMAIN: string;
+  export const PROJECT_ID: string;
+  export const STORAGE_BUCKET: string;
+  export const MESSAGING_SENDER_ID: string;
+  export const APP_ID: string;
+  export const LOCATION_REFRESH_RATE: string;
+}
