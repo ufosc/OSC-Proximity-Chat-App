09bbad473aaf46e0bb668756b76e7a8de02b1525
Connor Kress <con2coool@gmail.com>
2024-09-11 15:23:35 -0400

    Refactored update user API route and action

--- server/src/routes/user/update.ts
+++ server/src/routes/user/update.ts
@@ -1,50 +1,78 @@
 import { Router } from "express";
-import { toggleUserConnectionStatus, updateUserDisplayName, updateUserLocation } from "../../actions/updateConnectedUser";
+import {
+  toggleUserConnectionStatus,
+  updateUserDisplayName,
+  updateUserLocation
+} from "../../actions/updateConnectedUser";
 
 const updateUserRoute = Router();
 
 updateUserRoute.put("/users", async (req, res) => {
-    let query = "";
-    try {
-      if (req.query.userId && req.query.toggleConnection) {
-        // Note: toggleConnection should be assigned 'true', but it at least needs to contain any value. We don't perform a check on this parameter for this reason.
-        query = "?userId&toggleConnection";
-        const userId = req.query.userId;
-        if (typeof userId != "string") throw Error("  [userId] is not a string.");
-  
-        const success = await toggleUserConnectionStatus(userId);
-        if (!success) throw Error("     toggleUserConnectionStatus() failed.");
-      } else if (req.query.userId && req.query.lat && req.query.lon) {
-        query = "?userId&lat&lon";
-        const userId = req.query.userId;
-        const lat = Number(req.query.lat);
-        const lon = Number(req.query.lon);
-        if (typeof userId != "string") throw Error("  [userId] is not a string.");
-        if (typeof lat != "number") throw Error("  [lat] is not a number.");
-        if (typeof lon != "number") throw Error("  [lon] is not a number.");
-  
-        const success = await updateUserLocation(userId, lat, lon);
-        if (!success) throw Error("     toggleUserConnectionStatus() failed.");
-      } else if (req.query.userId && req.query.displayName) {
-        query = "?userId&displayName";
-        const userId = req.query.userId;
-        if (typeof userId != "string") throw Error("  [userId] is not a string.");
-        const displayName = req.query.displayName;
-        if (typeof displayName != "string")
-          throw Error("  [displayName] is not a string.");
-  
-        const success = await updateUserDisplayName(userId, displayName);
-        if (!success) throw Error("updateDisplayName() failed.");
-      }
-      console.log(`[EXP] Request <PUT /users${query}> returned successfully.`);
-      res.json(`Operation <PUT /users${query}> was handled successfully.`);
-    } catch (error) {
-      console.error(
-        `[EXP] Error returning request <PUT /users${query}>:\n`,
-        error.message
-      );
-      res.json(`Operation <PUT /user${query}> failed.`);
+  const baseErrorMsg = `[EXP] Error processing request <PUT ${req.url}>:\n\t`;
+  let userId = null;
+  if (req.query.userId) {
+    userId = req.query.userId;
+    if (typeof userId != "string") {
+      console.error(baseErrorMsg, "Invalid [userId].");
+      res.status(400).json(`Operation <PUT ${req.url}> failed.`);
+      return;
     }
-  });
+  }
+  if (req.query.userId && req.query.toggleConnection) {
+    // Note: toggleConnection should be assigned 'true', but it at least
+    // needs to contain any value. We don't perform a check on this
+    // parameter for this reason.
+    try {
+      await toggleUserConnectionStatus(userId);
+    } catch (error) {
+      console.error(baseErrorMsg, error.message);
+      res.status(500).json(`Operation <PUT ${req.url}> failed.`);
+      return;
+    }
+    res.json("Operation <PUT /users?userId&toggleConnection> was handled successfully.");
+  } else if (req.query.userId && req.query.lat && req.query.lon) {
+    let lat: number, lon: number;
+    try {
+      lat = Number(req.query.lat);
+      lon = Number(req.query.lon);
+    } catch {
+      console.error(baseErrorMsg, "[lat/lon] are not valid numbers.");
+      res.status(400).json(`Operation <PUT ${req.url}> failed.`);
+      return;
+    }
+
+    try {
+      await updateUserLocation(userId, lat, lon);
+    } catch (error) {
+      console.error(baseErrorMsg, error.message);
+      res.status(500).json(`Operation <PUT ${req.url}> failed.`);
+      return;
+    }
+    res.json("Operation <PUT /user?userId&lat&lon> was handled successfully.");
+  } else if (req.query.userId && req.query.displayName) {
+    const displayName = req.query.displayName;
+    if (typeof displayName != "string") {
+      console.error(baseErrorMsg, "Invalid [displayName].");
+      res.status(400).json(`Operation <PUT ${req.url}> failed.`);
+      return;
+    }
+    try {
+      await updateUserDisplayName(userId, displayName);
+    } catch (error) {
+      console.error(baseErrorMsg, error.message);
+      res.status(500).json(`Operation <PUT ${req.url}> failed.`);
+      return;
+    }
+    res.json("Operation <PUT /user?userId&displayName> was handled successfully.");
+  } else {
+    console.error(
+      `[EXP] Error returning request <PUT ${req.url}>:\n\t`,
+      "No valid target specification."
+    );
+    res.status(400).json(`Operation <PUT ${req.url}> failed.`);
+    return;
+  }
+  console.log(`[EXP] Request <PUT ${req.url}> returned successfully.`);
+});
 
 export default updateUserRoute;
