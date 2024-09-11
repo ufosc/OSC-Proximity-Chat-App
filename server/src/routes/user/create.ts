a084672e80df668dcce8ddd9aa2fe2f3e5029a90
Connor Kress <con2coool@gmail.com>
2024-09-11 13:16:51 -0400

    Refactored create user API route

--- server/src/routes/user/create.ts
+++ server/src/routes/user/create.ts
@@ -4,31 +4,36 @@ import { createUser } from "../../actions/createConnectedUser";
 const createUserRoute = Router();
 
 createUserRoute.post("/users", async (req, res) => {
-    try {
-      const status = await createUser({
-        uid: req.body.uid,
-        socketId: req.body.socketId,
-        displayName: req.body.displayName,
-        userIcon: {
-          foregroundImage: req.body.userIcon.foregroundImage,
-          backgroundImage: req.body.userIcon.backgroundImage,
-        },
-        location: {
-          lat: Number(req.body.location.lat),
-          lon: Number(req.body.location.lon),
-          geohash: req.body.location.geohash,
-        },
-      });
-      if (status === false) throw new Error("Error creating user: ");
-      res.json("Operation <POST /user> was handled successfully.");
-      console.log("[EXP] Request <POST /users> returned successfully.");
-    } catch (error) {
-      console.error(
-        "[EXP] Error returning request <POST /users>:\n",
-        error.message
-      );
-      res.json(`Operation <POST /user> failed.`);
-    }
+  let status: boolean;
+  try {
+    status = await createUser({
+      uid: req.body.uid,
+      socketId: req.body.socketId,
+      displayName: req.body.displayName,
+      userIcon: {
+        foregroundImage: req.body.userIcon.foregroundImage,
+        backgroundImage: req.body.userIcon.backgroundImage,
+      },
+      location: {
+        lat: Number(req.body.location.lat),
+        lon: Number(req.body.location.lon),
+        geohash: req.body.location.geohash,
+      },
+    });
+  } catch (error) {
+    console.error(
+      "[EXP] Error returning request <POST /users>:\n",
+      error.message
+    );
+    res.status(500).json(`Operation <POST /user> failed.`);
+  }
+  if (!status) {
+    console.log("[EXP] Request <POST /users> returned successfully.");
+    res.json("Operation <POST /user> was handled successfully.");
+  } else {
+    console.error("[EXP] Error returning request <POST /users>:\n");
+    res.status(500).json(`Operation <POST /user> failed.`);
+  }
 });
 
 export default createUserRoute;
