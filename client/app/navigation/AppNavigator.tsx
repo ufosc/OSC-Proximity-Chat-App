f11946b1dbbd9ac2dc92b459f7edba264b455fe6
eris6 <erispatel20@gmail.com>
2024-09-17 18:42:28 -0400

    Change home and profile icons, make text bigger

--- client/app/navigation/AppNavigator.tsx
+++ client/app/navigation/AppNavigator.tsx
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {StyleSheet, Text, View} from 'react-native';
 import * as React from "react";
 
 import { LocationProvider } from "../contexts/LocationContext";
@@ -6,8 +7,13 @@ import { SocketProvider } from "../contexts/SocketContext";
 import { UserProvider } from "../contexts/UserContext";
 import ChatScreen from "../screens/chat/ChatScreen";
 import SettingsScreen from "../screens/settings/SettingsScreen";
+import { Home } from "react-native-feather";
+import {User} from "react-native-feather";
 
 const Tab = createBottomTabNavigator();
+
+
+
 
 const AppNavigator = () => {
   return (
@@ -16,15 +22,43 @@ const AppNavigator = () => {
         <UserProvider>
           <Tab.Navigator
             screenOptions={{
+              
+          
               headerShown: false,
+              tabBarLabelStyle: {
+                fontSize: 15,
+              }
             }}>
-            <Tab.Screen name="Home" component={ChatScreen} />
-            <Tab.Screen name="Profile" component={SettingsScreen} />
+            <Tab.Screen name="Home" component={ChatScreen} options={{
+              
+              tabBarIcon:({ focused, color, size }) => {
+                return(<Home width = {size} stroke = {color}/>
+                )
+              }
+            }
+            }/>
+            <Tab.Screen name="Profile" component={SettingsScreen} options = {{
+              tabBarIcon:({ focused, color, size }) => {
+                return(<User width = {size} stroke = {color} />)
+              }
+            }
+            }
+            />
+            
+            
+            
           </Tab.Navigator>
         </UserProvider>
       </SocketProvider>
     </LocationProvider>
   );
 };
+
+
+
+
+
+
+
 
 export default AppNavigator;
