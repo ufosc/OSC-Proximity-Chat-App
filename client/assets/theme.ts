44e1230df3a6c1793e47c497b68966148dd5e28a
Venkata Suresh <user@USERs-iMac.local>
2024-09-19 21:18:09 -0400

    added Theme.ts file which contains all the hex values of the majors colors that are used

---
+++ client/assets/theme.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    gray     : "#F7F7F7",
+    green    : "#34D1BF",
+    white    : "#FFFFFF",
+    red      : "#FF2147",
+    black    : "#000000",
+    daryGray : "#333333",
+    blue     : "#3532D4"
+ }
