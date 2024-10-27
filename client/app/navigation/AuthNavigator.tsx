508ad213b7d8dc19b9e059f1fcf259005c7625bd
jqiu0601 <116924547+jqiu0601@users.noreply.github.com>
2024-10-26 23:25:33 -0400

    Update AuthNavigator.tsx
    Added Digit Verification to navigator

--- client/app/navigation/AuthNavigator.tsx
+++ client/app/navigation/AuthNavigator.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import LoginScreen from "../screens/auth/LoginScreen";
 import SignUpScreen from "../screens/auth/SignUpScreen";
 import WelcomeScreen from "../screens/auth/WelcomeScreen";
+import DigitVerificationScreen from "../screens/auth/DigitVerificationScreen";
 
 const Stack = createStackNavigator();
 
@@ -16,6 +17,7 @@ const AuthNavigator = () => {
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Log In" component={LoginScreen} />
       <Stack.Screen name="Sign Up" component={SignUpScreen} />
+      <Stack.Screen name="Digit Verification" component={DigitVerificationScreen} />
     </Stack.Navigator>
   );
 };
