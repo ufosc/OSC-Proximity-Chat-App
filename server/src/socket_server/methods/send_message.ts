f3427cb71a1c270e45e1ca504331239331daef8d
William Huffman <wehuffman1@gmail.com>
2025-01-09 16:35:11 -0500

    Fixed duplicate messages being sent

--- server/src/socket_server/methods/send_message.ts
+++ server/src/socket_server/methods/send_message.ts
@@ -7,12 +7,23 @@ export const sendMessage = (ctx: ConnectionContext, message: Message, ack: any):
     if (message.location === undefined) {
         message.location = ctx.user.location;
     }
+    if (message.location.lat > 90 || message.location.lat < -90 || message.location.lon > 180 || message.location.lon < -180) {
+        console.log("[WS] User tried to send message with invalid location.");
+        if (ack) ack("invalid_location");
+        return;
+    }
     message.timestamp = Date.now();
 
     const recipients = getActiveUsersInView(message.location);
+    const seen = new Set<string>(); // Geofire can return duplicates. Prevent sending message to the same user multiple times
     for (const recipient of recipients) {
         //if (recipient.uid == ctx.user.uid) continue; // skip sender
+
+        if (seen.has(recipient.uid)) continue;
+        seen.add(recipient.uid);
+
         recipient.socket.emit("message", message);
+        console.log("[WS] Sent message to user <" + recipient.uid + ">");
     }
 
     if (ack) ack("success");
