1058538cc27d45cf35fa2dec7c877b60664571bf
Phoenix <71522316+h1divp@users.noreply.github.com>
2025-02-08 19:31:06 +0900

    Upgrade to Expo SDK 52 (#315)
    * upgraded frontend
    
    * fixed connecting to server
    
    * upgrades packages

--- client/app/navigation/AuthNavigator.tsx
+++ client/app/navigation/AuthNavigator.tsx
@@ -5,28 +5,30 @@ import LoginScreen from "../screens/auth/LoginScreen";
 import SignUpScreen from "../screens/auth/SignUpScreen";
 import WelcomeScreen from "../screens/auth/WelcomeScreen";
 import EmailVerificationScreen from "../screens/auth/EmailVerificationScreen";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, NavigationIndependentTree } from "@react-navigation/native";
 import DisplayNameScreen from "@app/screens/auth/DisplayNameScreen";
 
 const Stack = createStackNavigator();
 
 const AuthNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Log In" component={LoginScreen} />
-        <Stack.Screen name="Sign Up" component={SignUpScreen} />
-        <Stack.Screen
-          name="Email Verification"
-          component={EmailVerificationScreen}
-        />
-        {/* <Stack.Screen name="Display Name" component={DisplayNameScreen} /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <NavigationIndependentTree>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name="Log In" component={LoginScreen} />
+          <Stack.Screen name="Sign Up" component={SignUpScreen} />
+          <Stack.Screen
+            name="Email Verification"
+            component={EmailVerificationScreen}
+          />
+          {/* <Stack.Screen name="Display Name" component={DisplayNameScreen} /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NavigationIndependentTree>
   );
 };
 
