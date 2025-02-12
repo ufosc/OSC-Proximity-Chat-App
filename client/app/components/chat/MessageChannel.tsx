eb879c64706d8808fecf3461015d37bba80a089c
GIRISH G <91957405+Girishg17@users.noreply.github.com>
2025-02-12 12:26:57 +0530

    added bottom sheet on long press on a chat (#332)
    * added bottom sheet on long press on a chat
    
    * Update README.md (#328)
    
    looks better
    
    * Adjusted NearbyHeader UI
    
    * Made spacing cleaner (#330)
    
    ---------
    
    Co-authored-by: Phoenix <71522316+h1divp@users.noreply.github.com>
    Co-authored-by: Andrei Rauta <rauta.andrei@ufl.edu>

--- client/app/components/chat/MessageChannel.tsx
+++ client/app/components/chat/MessageChannel.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList } from "react-native";
 
 import Message from "./ChatMessage";
 import { MessageChannelProps } from "../../types/Props";
+import { TouchableOpacity } from "react-native";
+import SheetModal from "./SheetModal";
 
 const MessageChannel: React.FC<MessageChannelProps> = ({
   nearbyUsers,
   messages,
 }) => {
+  const [isModalVisible, setModalVisible] = useState(false);
   const reverseMessages = [...messages].reverse();
 
+
+  const handleLongPress = () => {
+    setModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+  };
+
   return (
+    <>
     <FlatList
       contentContainerStyle={{
         width: "100%",
@@ -21,16 +34,20 @@ const MessageChannel: React.FC<MessageChannelProps> = ({
         // console.log(nearbyUsers);
         if (user === undefined) return null;
         return (
+          <TouchableOpacity onLongPress={ handleLongPress }>
           <Message
             messageContent={item.content.text!}
             author={user.displayName}
             time={item.timestamp}
           />
+          </TouchableOpacity>
         );
       }}
       inverted // This will render items from the bottom
       onLayout={() => {}} // This will make sure the list is scrolled to the bottom on first render
     />
+{isModalVisible && <SheetModal isVisible={isModalVisible} onDismiss={handleCloseModal} />}
+</>
   );
 };
 
